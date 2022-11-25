05c79fe709fc8112f4c4dac56dce31b5a3b7ffae
Prakhar Dubey <prakhardubey2002@gmail.com>
2022-11-25 22:05:02 +0530

    duckduckgo based scrapping added

--- src/index.js
+++ src/index.js
@@ -10,31 +10,62 @@ bot.on('message', (msg) => {
     const chatId = msg.chat.id;
     console.log(msg);
     // send a message to the chat acknowledging receipt of their message
-    let word = msg.text.includes("git")
-    console.log(word);
+    // let word = msg.text.includes("git")
+    // console.log(word);
     const wordex = (msg.text.replace(' ', '+'))
     console.log(wordex);
-    if (word) {
+    if (wordex) {
 
-        var a = nightmare
-            // .goto(`https://stackoverflow.com/questions/tagged/${msg.text}`)
-            .goto(`https://stackoverflow.com/search?q=${wordex}`)
-            .wait('.s-post-summary--content-excerpt')
-            // .evaluate(() => document.querySelector('.mb24').innerText)
-            .evaluate(() => document.querySelector('.s-post-summary--content-excerpt').innerText)
+        // var a = nightmare
+        //     // .goto(`https://stackoverflow.com/questions/tagged/${msg.text}`)
+        //     .goto(`https://stackoverflow.com/search?q=${wordex}`)
+        //     .wait('.s-post-summary--content-excerpt')
+        //     // .evaluate(() => document.querySelector('.mb24').innerText)
+        //     .evaluate(() => document.querySelector('.s-post-summary--content-excerpt').innerText)
+        //     .then(
+        //         (res) => {
+
+        //             bot.sendMessage(chatId, `HI ${msg.chat.first_name} for you question ${msg.text}  relavent question on stackoverflow is : ${res}`);
+                    
+        //         }
+
+        //         // console.log
+
+        //     )
+        //     .catch(error => {
+        //         console.log(error);
+        //     })
+            // var b = nightmare.goto(`https://duckduckgo.com/?q=${wordex}&t=h_&ia=web`)
+            // .wait('.js-about-item-abstr')
+            // .evaluate(() => document.querySelector('.js-about-item-abstr').innerText)
+            // .then(
+            //     (res) => {
+            //         console.log("res2")
+
+            //         bot.sendMessage(chatId, `HI ${msg.chat.first_name} for you question ${msg.text}  relavent answer is : ${res}`);
+                    
+            //     }
+
+            // )
+            // .catch(error => {
+            //     console.log(error);
+            // })
+            var b = nightmare.goto(`https://duckduckgo.com/?q=${wordex}&t=h_&ia=web`)
+            .wait('.js-about-item-abstr')
+            .evaluate(() => document.querySelector('.OgdwYG6KE2qthn9XQWFC').innerText)
             .then(
-                console.log
+                (res) => {
+                    console.log("res2")
+
+                    bot.sendMessage(chatId, `HI ${msg.chat.first_name} your relavent answer is : ${res}`);
+                    
+                }
 
             )
             .catch(error => {
                 console.log(error);
             })
 
-
-        if (typeof a === "string") {
-
-            bot.sendMessage(chatId, `HI ${msg.chat.first_name} for you question ${msg.text} opening relavent question on stackoverflow${a}`);
-        }
 
 
     }
